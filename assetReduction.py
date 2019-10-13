4d5e0f0b82538f5420a3830778ba2acb2d60f61c
Abdullah Aljarboua <abdullah.jarboua@kapsarc.org>
2019-10-13 16:55:58 +0300

    Fixed few bugs in calculating the OPEX for finding projects in the same range. Added approval year for new combined assets. Needs more implementation

--- assetReduction.py
+++ assetReduction.py
@@ -1,125 +1,173 @@
 import pickle
 import pandas as pd
 import numpy as np
+import gdxpds
 
 with open('rystad.panda', 'rb') as f:
-    asset,approval,breakEven,OPEX,annualCapex,production,RM = pickle.load(f)
+    asset, country, group, assetType, approval, start_y, last_y, breakEven, CAPEX_total, max_production, OPEX, annualCapex, production, RM = pickle.load(f)
 
-def wavg(values,weights,axis=0):
+
+def wavg(values, weights, axis=0):
     try:
         return (values * weights).sum(axis=axis) / weights.sum(axis=axis)
     except ZeroDivisionError:
         return values.mean(axis=1)
 
-def wstd(values,weights,axis=0):
+
+def wstd(values, weights, axis=0):
     try:
-        return (((weights*values.subtract(wavg(values,weights,axis=axis),axis=0)**2).sum(axis=axis))/
-        	((weights[weights>0].count(axis=axis).subtract(1))/weights[weights>0].count(axis=axis) * weights.sum(axis=axis)))**0.5
+        return (((weights * values.subtract(wavg(values, weights, axis=axis), axis=0) ** 2).sum(axis=axis)) /
+                ((weights[weights > 0].count(axis=axis).subtract(1)) / weights[weights > 0].count(
+                    axis=axis) * weights.sum(axis=axis))) ** 0.5
     except ZeroDivisionError:
-        return values.std(axis=1)	
+        return values.std(axis=1)
 
-def appendRows(output,df,years,i,group):
-	# output:	panda DataFrame (pivot_table) to append row to
-	# df:		intput DataFrame to append as pivot_table rows
-	# years: 	years as column headings for pivot_table
-	# i.        integer number to index new aggregate asst group
-	# group 	identifier for AssetGroup (e.g. tight oil)
-	assetStr = 'newAsset'+group+str(i)
-	n = len(years)
-	df = pd.DataFrame(data={'Value': df.values, 
-		"Asset": [assetStr]*n, 
-		"Country": ['Global']*n, 'Type': ['All']*n, 'Group': [group]*n, 'Year': years})
-	rows= pd.pivot_table(df, values='Value', index=['Asset', 'Country', 'Type', 'Group'], columns='Year')
-	return output.append(rows)
 
-def costAggregator(output,costRange,costs,group='Other'): 
-	# output: 	 panda DataFrame where the aggregate values are stored
-	# costRange: cost bin ranges to aggregate over
-	# costs: 	 filterd cost table to aggregate. e.g. existing assets conventional...
-	# group: 	 asset group name (defaults to other). 
-	
-	yearRange = slice(119,200)  #take price data from year 2019 onluy
-	costs = costs.iloc[:,yearRange]
-	rows = costs.index.values # row index values (asset info)
-	cols = costs.columns # row column values (years)
-	print('aggregating asset group '+str(group))
+def appendRows(output, df, years, i, group):
+    # output:	panda DataFrame (pivot_table) to append row to
+    # df:		intput DataFrame to append as pivot_table rows
+    # years: 	years as column headings for pivot_table
+    # i.        integer number to index new aggregate asst group
+    # group 	identifier for AssetGroup (e.g. tight oil)
+    global approval
+    assetStr = 'newAsset' + group + str(i)
+    n = len(years)
+    df = pd.DataFrame(data={'Value': df.values,
+                            "Asset": [assetStr] * n,
+                            "Country": ['Global'] * n, 'Type': ['All'] * n, 'Group': [group] * n, 'Year': years})
+    rows = pd.pivot_table(df, values='Value', index=['Asset', 'Country', 'Type', 'Group'], columns='Year')
 
-	global production,annualCapex
+    # Adding approval_year for new assets
+    if 'New' in group:
+        approval.append({'Asset': assetStr, 'Value': float(group[-4:])}, ignore_index=True)
 
-	#	use production as weights
-	weights = production.loc[rows,cols]
-	# 	Adjust costs to account for price markers
-	costs = costs.divide(RM.loc[rows,cols])
+    return output.append(rows)
 
-	assetGroup = []
-	assetsToDrop = []
-	for i in range(len(costRange)):
-		if i == len(costRange)-1:
-			break	
 
-		tmp = costs[(costs>=costRange[i]) & (costs<costRange[i+1])]
-		n = tmp.count()
-		if n.any():
-			print('aggregating '+str(costRange[i])+' to '+str(costRange[i+1]))
-			# list of current assets
-			assetGroup = tmp.index.values
+def costAggregator(output, costRange, costs, group='Other'):
+    # output: 	 panda DataFrame where the aggregate values are stored
+    # costRange: cost bin ranges to aggregate over
+    # costs: 	 filterd cost table to aggregate. e.g. existing assets conventional...
+    # group: 	 asset group name (defaults to other).
 
-			years = tmp.columns.values
-			# weighted average of assetGroup
-			tmp = wavg(tmp,weights.loc[assetGroup]) 
-			output = appendRows(output,tmp,years,i,group)
+    yearRange = slice(119, 200)  # take price data from year 2019 onluy
+    costs = costs.iloc[:, yearRange]
+    rows = costs.index.values  # row index values (asset info)
+    cols = costs.columns  # row column values (years)
+    print('aggregating asset group ' + str(group))
 
-			# aggregate production
-			tmp = production.loc[rows,cols].sum(axis=0)
-			production = appendRows(production,tmp,years,i,group)
+    global production, annualCapex
 
-			# aggregate annual capex
-			tmp = annualCapex.loc[rows,cols].sum(axis=0)
-			annualCapex = appendRows(annualCapex,tmp,years,i,group)
+    #	use production as weights
+    weights = production.loc[rows, cols]
+    # 	Adjust costs to account for price markers
+    costs = costs.divide(RM.loc[rows, cols])
 
-			#assets to drop from global list
-			assetsToDrop += list(set().union(assetGroup, assetsToDrop))
-			 
-	production = production.drop(assetsToDrop)
-	#annualCapex = annualCapex.drop(assetsToDrop)
-	return output.drop(assetsToDrop)
+    assetGroup = []
+    assetsToDrop = []
+    for i in range(len(costRange)):
+        if i == len(costRange) - 1:
+            break
 
-countriesOPEC = ['Saudi Arabia','Iran','Iraq','Kuwait','Qatar','UAE','Libya',
-	'Algeria','Nigeria','Equatorial Guinea',
-    'Gabon','Congo','Angola','Ecuador',
-    'Venezuela']
+        tmp = costs[(costs.mean(axis=1) >= costRange[i]) & (costs.mean(axis=1) < costRange[i + 1])]
+        n = tmp.count()
+        if n.any():
+            print('aggregating ' + str(costRange[i]) + ' to ' + str(costRange[i + 1]))
 
-#OPEX['avg'] = wavg(_OPEX,_production,axis=1)
-#OPEX['std'] = wstd(_OPEX,_production,axis=1) 
-#OPEX['max'] = _OPEX.max(axis=1)
-#OPEX['min'] = _OPEX.min(axis=1) 
+            # list of current assets
+            assetGroup = tmp.index.values
+
+            years = tmp.columns.values
+            # weighted average of assetGroup
+            tmp = wavg(tmp, weights.loc[assetGroup])
+            output = appendRows(output, tmp, years, i, group)
+
+            # aggregate production
+            tmp = production.loc[assetGroup, cols].sum(axis=0)
+            production = appendRows(production, tmp, years, i, group)
+
+            # aggregate annual capex
+            tmp = annualCapex.loc[assetGroup, cols].sum(axis=0)
+            annualCapex = appendRows(annualCapex, tmp, years, i, group)
+
+            # assets to drop from global list
+            assetsToDrop += list(set().union(assetGroup, assetsToDrop))
+
+    production = production.drop(assetsToDrop)
+    # annualCapex = annualCapex.drop(assetsToDrop)
+    return output.drop(assetsToDrop)
+
+
+countriesOPEC = ['Saudi Arabia', 'Iran', 'Iraq', 'Kuwait', 'Qatar', 'UAE', 'Libya',
+                 'Algeria', 'Nigeria', 'Equatorial Guinea',
+                 'Gabon', 'Congo', 'Angola', 'Ecuador',
+                 'Venezuela']
+
+# OPEX['avg'] = wavg(_OPEX,_production,axis=1)
+# OPEX['std'] = wstd(_OPEX,_production,axis=1)
+# OPEX['max'] = _OPEX.max(axis=1)
+# OPEX['min'] = _OPEX.min(axis=1)
 
 # only projects approved before 2019
-existingAssets = approval.loc[approval["Value"]<2019]['Asset'].values
+existingAssets = approval.loc[approval["Value"] < 2019]['Asset'].values
 
-
-#cost bins to iterate over
-costRange = [0,10,30,40,50]
+# cost bins to iterate over
+costRange = [0, 10, 30, 40, 50]
 costRange += np.arange(51, 120, 1).tolist()
+# Adding the maximum mean to the list
+costRange.append(OPEX.divide(RM).mean(axis=1).max())
 
-#drop OPEC
-costs = OPEX.drop(index=countriesOPEC,level=1)
-#drop tight oil
-costs = costs.drop(index=['Tight Oil'],level=3)
+# drop OPEC
+costs = OPEX.drop(index=countriesOPEC, level=1)
+# drop tight oil
+costs = costs.drop(index=['Tight Oil'], level=3)
+costs = costs.drop(index=['Tight oil'], level=3)
 
 # select costs for existing assets only
 tmp = costs.loc[existingAssets]
-OPEX = costAggregator(OPEX,costRange,tmp,'ExistConv')
-for year in np.arange(2019,2050,1).tolist():
-	# only projects approved after 2019
-	newAssets = approval.loc[approval["Value"]==year]['Asset'].values
-	# select costs for new assets 
-	tmp = costs.loc[newAssets]
-	OPEX = costAggregator(OPEX,costRange,tmp,'NewConv'+str(year))
-	
+OPEX = costAggregator(OPEX, costRange, tmp, 'ExistConv')
+for year in np.arange(2019, 2050, 1).tolist():
+    # only projects approved after 2019
+    newAssets = approval.loc[approval["Value"] == year]['Asset'].values
+    # select costs for new assets
+    tmp = costs.loc[newAssets]
+    OPEX = costAggregator(OPEX, costRange, tmp, 'NewConv' + str(year))
+
 print(OPEX)
 
-#TO-DO
-#select tight oil break even costs
-#tmp = costs.select(index=['Tight Oil'],level=3)
-#breakEven = costAggregator(breakEven,costRange,tmp,'ExistingConv')
+# TO-DO
+# select tight oil break even costs
+# tmp = costs.select(index=['Tight Oil'],level=3)
+# breakEven = costAggregator(breakEven,costRange,tmp,'ExistingConv')
+
+
+asset_list, country_list, assetType_list, group_list = zip(*OPEX.index.to_list())
+asset = pd.DataFrame({'Asset': list(set(asset_list)), 'Value': True})
+country = pd.DataFrame({'Asset': list(set(country_list)), 'Value': True})
+assetType = pd.DataFrame({'Asset': list(set(assetType_list)), 'Value': True})
+group = pd.DataFrame({'Asset': list(set(group_list)), 'Value': True})
+
+OPEX.index.names = ['asset', 'c', 'type', 'group']
+OPEX.columns.names = ['time']
+production.index.names = ['asset', 'c', 'type', 'group']
+production.columns.names = ['time']
+
+max_production_GDX = production.max(axis=1).to_frame()
+max_production_GDX.columns = ['Value']
+max_production_GDX = max_production_GDX.reset_index()
+
+OPEX_GDX = OPEX.stack().to_frame()
+OPEX_GDX.columns = ['Value']
+OPEX_GDX = OPEX_GDX.reset_index()
+prod_GDX = production.stack().to_frame()
+prod_GDX.columns = ['Value']
+prod_GDX = prod_GDX.reset_index()
+
+data_ready_for_GAMS = {'asset': asset, 'country': country, 'group': group, 'type': assetType,
+                       'approval': approval, 'start': start_y, 'last_year': last_y,
+                        'max_production': max_production_GDX, 'CAPEX_total': CAPEX_total, 'breakeven': breakEven,
+                       'OPEX_pr_bbl':  OPEX_GDX, 'production': prod_GDX
+                        # , 'CAPEX_annual': annualCapex
+                        }
+gdx = gdxpds.to_gdx(data_ready_for_GAMS, path='data_to_send_to_gams.gdx')
+production.to_excel('test.xlsx')
--- readGDX.py
+++ readGDX.py
@@ -1,6 +1,5 @@
 import gdxpds
 import pandas as pd
-import numpy
 import pickle
 
 if __name__ == "__main__":
@@ -12,12 +11,30 @@ if __name__ == "__main__":
     print('Finished opening the GDX')
 
     # Loading the parameters from the dataFrame and setting the headers names
+    # Loading sets
     asset = tables['asset']
     asset.columns = ['Asset', 'Value']
+    country = tables['country']
+    country.columns = ['c', 'Value']
+    group = tables['group']
+    group.columns = ['group', 'Value']
+    assetType = tables['type']
+    assetType.columns = ['type', 'Value']
+
+    # Loading parameters
     approval = tables['approval']
     approval.columns = ['Asset', 'Value']
+    start_y = tables['start']
+    start_y.columns = ['Asset', 'Value']
+    last_y = tables['last_year']
+    last_y.columns = ['Asset', 'Value']
+
     breakEven = tables['breakeven']
     breakEven.columns = ['Asset', 'Country', 'Type', 'Group', 'Value']
+    CAPEX_total = tables['CAPEX_total']
+    CAPEX_total.columns = ['Asset', 'Country', 'Type', 'Group', 'Value']
+    max_production = tables['max_production']
+    max_production.columns = ['Asset', 'Country', 'Type', 'Group', 'Value']
     _annualCapex = tables['CAPEX_annual']
     _annualCapex.columns = ['Asset', 'Country', 'Type', 'Group', 'Year', 'Value']
     _OPEX = tables['OPEX_pr_bbl']
@@ -38,7 +55,10 @@ if __name__ == "__main__":
     del _annualCapex, _OPEX, _production, _RM
 
     with open('rystad.panda', 'wb') as f:
-        pickle.dump([asset,approval,breakEven,OPEX,annualCapex,production,RM], f)
+        pickle.dump([asset, country, group, assetType,
+                     approval, start_y, last_y,
+                     breakEven, CAPEX_total, max_production,
+                     OPEX, annualCapex, production, RM], f)
 
     # Ready for data manipulation
     print('Data stored in pickle [asset,approval,breakEven,OPEX,annualCapex,production,RM]')
